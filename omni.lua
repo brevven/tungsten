35cb6bb074b60e10d30893e04bb9233f88bb8688
Brevven <brevingame@gmail.com>
2021-05-20 02:56:32 -0700

    omni whenever

--- omni.lua
+++ omni.lua
@@ -1,6 +1,3 @@
-if (not mods["pyrawores"] and not mods["bobplates"] and not mods["angelssmelting"]) then
 if omni and omni.matter then
   omni.matter.add_resource("tungsten-ore", omni.matter.get_ore_tier("iron-ore"))
 end
-end
-
