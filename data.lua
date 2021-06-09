c69e77d955500b780ed138994de4f624b00e93ba
Brevven <brevingame@gmail.com>
2021-06-09 02:52:04 -0700

    menu sim

--- data.lua
+++ data.lua
@@ -5,3 +5,4 @@ require("tungsten-enriched")   -- Enriched for Krastorio 2
 require("tungsten-recipe-se")  -- Space Exploration special recipes (depends on K2 if present)
 require("tungsten-compressed")
 require("advanced-carbon-furnace")
+require("tungsten-sim") -- menu simulation
