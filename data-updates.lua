fd225e9ab2d44c1d9efe162dd0096728a8d2d30c
Brevven <brevingame@gmail.com>
2022-08-01 14:39:30 -0700

    asteroid mining

--- data-updates.lua
+++ data-updates.lua
@@ -4,3 +4,10 @@ require("omni")
 require("map-gen-preset-updates")
 require("tungsten-recipe-5d")
 require("strange-matter")
+
+local util = require("data-util");
+
+if mods.Asteroid_Mining then
+  require("__Asteroid_Mining__.scripts.addmodule")
+  amfunc.addtype("tungsten-ore", {a = 0,r = 0.55,g = 0.60,b = 0.5})
+end
