dbb238b9c5de02c57db0608e696fb3b2a868b944
Brevven <brevingame@gmail.com>
2022-08-11 00:00:33 -0700

    aai oil proc

--- data-updates.lua
+++ data-updates.lua
@@ -8,6 +8,10 @@ require("compatibility/248k")
 
 local util = require("data-util");
 
+if mods["aai-industry"] then
+  util.add_prerequisite("oil-processing", "tungsten-processing")
+end
+
 if mods.Asteroid_Mining then
   require("__Asteroid_Mining__.scripts.addmodule")
   amfunc.addtype("tungsten-ore", {a = 0,r = 0.55,g = 0.60,b = 0.5})
